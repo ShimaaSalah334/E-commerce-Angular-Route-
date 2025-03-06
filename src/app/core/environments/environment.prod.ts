1ac92b0dc680012047c480a0cbe83fea6b82c3fc
ShimaaSalah334 <shimaaasalah2002@gmail.com>
2025-03-06 23:49:53 +0200

    23th commit

--- src/app/core/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  baseUrl: `https://ecommerce.routemisr.com`,
-  redirectUrl: 'https://e-commerce-angular-route.vercel.app/allorders' // Replace with your actual domain
-};
